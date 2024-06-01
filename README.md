b664dea28efef45dceb6fe620092083c595cb670
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2024-05-31 21:47:17 -0600

    Tarea7

---
+++ README.md
@@ -0,0 +1,3 @@
+# fabiolalainez_tarea7
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fabiolalainez_tarea7/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fabiolalainez_tarea7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
